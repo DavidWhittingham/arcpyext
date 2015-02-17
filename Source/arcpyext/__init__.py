7922999e9535b196396b18456dc965751ff225c2
David Whittingham <DavidWhittingham@users.noreply.github.com>
2015-02-17 15:56:08 +1000

    Bumped version number for future release.

--- Source/arcpyext/__init__.py
+++ Source/arcpyext/__init__.py
@@ -3,7 +3,7 @@
 Python Library that extends the Esri ArcPy library with useful helper functions.
 """
 
-__version__   = "0.3.0"
+__version__   = "0.3.1"
 __author__    =  "David Whittingham; David Payne"
 __copyright__ = "Copyright (C) 2015 David Whittingham"
 
