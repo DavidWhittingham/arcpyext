f3dc579ca3f3b0496e9a9c70f7b6c34b2a7f5ed9
David Whittingham <DavidWhittingham@users.noreply.github.com>
2015-05-19 16:12:51 +1000

    Updated GPSDDraft with new extensions support.

--- Source/arcpyext/publishing/_gpsddraft.py
+++ Source/arcpyext/publishing/_gpsddraft.py
@@ -1,6 +1,8 @@
-from ._sddraft_base import SDDraftBase
-
 from enum import Enum
+
+from ._sddraft_base import SDDraftBase
+from ._wps_server_extension import WpsServerExtension
+
 
 class GPSDDraft(SDDraftBase):
 
@@ -17,19 +19,21 @@ class GPSDDraft(SDDraftBase):
         warning = "Warning"
         info = "Info"
 
-    class Extension(Enum):
-        wpsserver = "WPSServer"
-
     #Override. MaxRecordCount key is completely different for a GP Service Draft. Why Esri, Why????
     _MAX_RECORD_COUNT_KEY = "maximumRecords"
 
     def __init__(self, editor):
         super(GPSDDraft, self).__init__(editor)
+        self._wps_server_extension = WpsServerExtension(editor)
 
     #region Properties
 
     @property
     def execution_type(self):
+        """Gets or sets the execution level for the service (as defined by the GPSDDraft.ExecutionType enumerated 
+        type).
+        
+        """
         return self.ExecutionType(self._editor.get_element_value(self._execution_type_elements[0]))
 
     @execution_type.setter
@@ -40,8 +44,37 @@ class GPSDDraft(SDDraftBase):
             self._editor.set_element_value(elem, value.value)
 
     @property
+    def folder(self):
+        """Gets or sets the name of the folder that the service will reside in."""
+        return super(GPSDDraft, self).folder
+
+    @folder.setter
+    def folder(self, value):
+        existing_full_path = self._get_full_path()
+        SDDraftBase.folder.fset(self, value)
+        self._set_full_path_properties(existing_full_path, self._get_full_path())
+
+    @property
+    def name(self):
+        """Gets or sets the name of the service."""
+        return super(GPSDDraft, self).name
+
+    @name.setter
+    def name(self, value):
+        existing_full_path = self._get_full_path()
+
+        # Set the underlying value from base class and then update properties that are built from the full path
+        SDDraftBase.name.fset(self, value)
+        self._set_full_path_properties(existing_full_path, self._get_full_path())
+
+    @property
     def result_map_server(self):
-        return self._editor.value_to_boolean(self._editor.get_element_value(self._result_map_server_elements[0]))
+        """Gets or sets a value indicating whether or not the results should be viewed with a map service.
+        
+        :type: bool
+        """
+        return self._editor.value_to_boolean(
+            self._editor.get_element_value(self._result_map_server_elements[0]))
 
     @result_map_server.setter
     def result_map_server(self, value):
@@ -51,6 +84,10 @@ class GPSDDraft(SDDraftBase):
 
     @property
     def show_messages(self):
+        """Gets or sets a value indicating what level of messaging should be shown with the service.
+        
+        :type: GPSDDraft.MessageLevel
+        """
         return self.MessageLevel(self._editor.get_element_value(self._show_messages_elements[0]))
 
     @show_messages.setter
@@ -59,6 +96,10 @@ class GPSDDraft(SDDraftBase):
             value = self.MessageLevel(value)
         for elem in self._show_messages_elements:
             self._editor.set_element_value(elem, value.value)
+
+    @property
+    def wps_server(self):
+        return self._wps_server_extension
 
     @property
     def _execution_type_elements(self):
@@ -122,5 +163,20 @@ class GPSDDraft(SDDraftBase):
         elems = super(GPSDDraft, self)._wait_timeout_elements
         elems.append(self._editor.get_value_element_by_key(self._config_props, self._WAIT_TIMEOUT_KEY))
         return elems
+
+    #endregion
+
+    #region Methods
+
+    def _set_full_path_properties(self, old_path, new_path):
+        """Sets the value for properties that are dependent on both the folder name and the service name.
+
+        Only sets the values if they haven't been changed from the default "folder_name" value.
+        """
+        if self.wps_server.app_schema_prefix == old_path:
+            self.wps_server.app_schema_prefix = new_path
+
+    def _get_full_path(self):
+        return "{0}_{1}".format(self.folder, self.name) if self.folder != None else self.name
 
     #endregion
---
+++ Source/arcpyext/publishing/_wps_server_extension.py
@@ -0,0 +1,153 @@
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+from builtins import (ascii, bytes, chr, dict, filter, hex, input, int, map, next, oct, open, pow,
+                      range, round, str, super, zip)
+
+from ._sddraft_extension import SDDraftExtension
+from ._ogc_metadata_extension_mixin import OgcMetadataExtensionMixin
+from ._custom_get_capabilities_extension_mixin import CustomGetCapabilitiesExtensionMixin
+
+from enum import Enum
+
+
+class WpsServerExtension(CustomGetCapabilitiesExtensionMixin, OgcMetadataExtensionMixin, SDDraftExtension):
+
+    _ADMINISTRATIVE_AREA_KEY = "administrativeArea"
+    _ADDRESS_KEY = "deliveryPoint"
+    _CONTACT_INSTRUCTIONS_KEY = "contactInstructions"
+    _EMAIL_KEY = "electronicMailAddress"
+    _EXTENSION_TYPE = "WPSServer"
+    _KEYWORD_KEY = "keyword"
+    _KEYWORDS_TYPE_KEY = "keywordsType"
+    _FACSIMILE_KEY = "facsimile"
+    _FEES_KEY = "fee"
+    _HOURS_OF_SERVICE_KEY = "hoursOfService"
+    _INDIVIDUAL_NAME_KEY = "individualName"
+    _NAME_KEY = "name"
+    _ORGANIZATION_KEY = "providerName"
+    _PHONE_KEY = "phone"
+    _POSITION_NAME_KEY = "positionName"
+    _POST_CODE_KEY = "postalCode"
+    _PREFIX_KEY = "appSchemaPrefix"
+    _PROFILE_KEY = "profile"
+    _PROVIDER_SITE_KEY = "providerSite"
+    _ROLE_KEY = "role"
+    _SERVICE_TYPE_KEY = "serviceType"
+    _SERVICE_TYPE_VERSION_KEY = "serviceTypeVersion"
+
+    @property
+    def app_schema_prefix(self):
+        """Gets or sets the WFS namespace prefix that is used by the WFS GetFeatureRequest request.
+
+        :type: str
+        """
+        return self._get_prop_value(self._PREFIX_KEY)
+
+    @app_schema_prefix.setter
+    def app_schema_prefix(self, value):
+        self._set_prop_value(self._PREFIX_KEY, value)
+
+    @property
+    def capabilities(self):
+        """JPIP Server Extension does not support capabilities.  Returns None, raises NotImplementedError on set."""
+        return None
+
+    @capabilities.setter
+    def capabilities(self, values):
+        raise NotImplementedError("No capabilities can be set for the JPIP Server Extension.")
+
+    @property
+    def contact_instructions(self):
+        """Gets or sets instructions for appropriately contacting the organization and/or named person responsible
+        for the service.
+
+        :type: str
+        """
+        return self._get_prop_value(self._CONTACT_INSTRUCTIONS_KEY)
+
+    @contact_instructions.setter
+    def contact_instructions(self, value):
+        self._set_prop_value(self._CONTACT_INSTRUCTIONS_KEY, value)
+
+    @property
+    def hours_of_service(self):
+        """Gets or sets the WFS namespace prefix that is used by the WFS GetFeatureRequest request.
+
+        :type: str
+        """
+        return self._get_prop_value(self._HOURS_OF_SERVICE_KEY)
+
+    @hours_of_service.setter
+    def hours_of_service(self, value):
+        self._set_prop_value(self._HOURS_OF_SERVICE_KEY, value)
+
+    @property
+    def keywords_type(self):
+        """Gets or sets the keyword type value for the service.
+
+        :type: str
+        """
+        return self._get_prop_value(self._KEYWORDS_TYPE_KEY)
+
+    @keywords_type.setter
+    def keywords_type(self, value):
+        self._set_prop_value(self._KEYWORDS_TYPE_KEY, value)
+
+    @property
+    def profile(self):
+        """Gets or sets the profile metadata value for the service.
+
+        :type: str
+        """
+        return self._get_prop_value(self._PROFILE_KEY)
+
+    @profile.setter
+    def profile(self, value):
+        self._set_prop_value(self._PROFILE_KEY, value)
+
+    @property
+    def provider_site(self):
+        """Gets or sets a URL to the provider's website.
+
+        :type: str
+        """
+        return self._get_prop_value(self._PROVIDER_SITE_KEY)
+
+    @provider_site.setter
+    def provider_site(self, value):
+        self._set_prop_value(self._PROVIDER_SITE_KEY, value)
+
+    @property
+    def role(self):
+        """Gets or sets the role metadata value for the service.
+
+        :type: str
+        """
+        return self._get_prop_value(self._ROLE_KEY)
+
+    @role.setter
+    def role(self, value):
+        self._set_prop_value(self._ROLE_KEY, value)
+
+    @property
+    def service_type(self):
+        """Gets or sets the service type for the service, e.g. 'WPS'.
+
+        :type: str
+        """
+        return self._get_prop_value(self._SERVICE_TYPE_KEY)
+
+    @service_type.setter
+    def service_type(self, value):
+        self._set_prop_value(self._SERVICE_TYPE_KEY, value)
+
+    @property
+    def service_type_version(self):
+        """Gets or sets the service type version for the service, e.g. '1.0'.
+
+        :type: str
+        """
+        return self._get_prop_value(self._SERVICE_TYPE_VERSION_KEY)
+
+    @service_type_version.setter
+    def service_type_version(self, value):
+        self._set_prop_value(self._SERVICE_TYPE_VERSION_KEY, value)
