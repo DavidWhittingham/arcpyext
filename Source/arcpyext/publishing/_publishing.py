0cc4193a2e5e86ac9c6d431deabd6dc8b97f61b3
Adam Kerz <adam.kerz@dnrm.qld.gov.au>
2015-06-18 16:39:47 +1000

    Fixing bugs not tested properly before pushing

--- Source/arcpyext/publishing/_publishing.py
+++ Source/arcpyext/publishing/_publishing.py
@@ -10,19 +10,20 @@ from ._imagesddraft import ImageSDDraft
 from ._gpsddraft import GPSDDraft
 from ._sddraft_editor import SDDraftEditor
 
+
+def check_analysis(analysis):
+    if not analysis["errors"] == {}:
+        err_message_list = []
+        for ((message, code), layerlist) in analysis["errors"].iteritems():
+            if layerlist == None:
+                err_message_list.append("{message} (CODE {code})".format(message = message, code = code))
+            else:
+                err_message_list.append("{message} (CODE {code}) applies to: {layers}".format(
+                                            message = message, code = code, layers = ", ".join([layer.name for layer in layerlist])))
+        raise ServDefDraftCreateError("Analysis Errors: \n{errs}".format(errs = "\n".join(err_message_list)))
+
+
 def convert_map_to_service_draft(map, sd_draft_path, service_name, folder_name = None, summary = None):
-
-    def check_analysis(analysis):
-        if not analysis["errors"] == {}:
-            err_message_list = []
-            for ((message, code), layerlist) in analysis["errors"].iteritems():
-                if layerlist == None:
-                    err_message_list.append("{message} (CODE {code})".format(message = message, code = code))
-                else:
-                    err_message_list.append("{message} (CODE {code}) applies to: {layers}".format(
-                                                message = message, code = code, layers = ", ".join([layer.name for layer in layerlist])))
-            raise ServDefDraftCreateError("Analysis Errors: \n{errs}".format(errs = "\n".join(err_message_list)))
-
     if not validate_map(map):
         raise MapDataSourcesBrokenError()
 
@@ -49,10 +50,11 @@ def convert_toolbox_to_service_draft(toolbox_path, sd_draft_path, get_result_fn,
         result=get_result_fn()
 
     # create the sddraft
-    arcpy.CreateGPSDDraft(result, sd_draft_path, service_name, server_type="ARCGIS_SERVER", copy_data_to_server=False, folder_name=folder_name, summary=summary)
+    analysis=arcpy.CreateGPSDDraft(result, sd_draft_path, service_name, server_type="ARCGIS_SERVER", copy_data_to_server=False, folder_name=folder_name, summary=summary)
+    check_analysis(analysis)
     # and analyse it
     analysis=arcpy.mapping.AnalyzeForSD(sd_draft_path)
-    _check_analysis(analysis)
+    check_analysis(analysis)
 
     return load_gp_sddraft(sd_draft_path)
 
