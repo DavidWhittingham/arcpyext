42403cc6d7b863d7fa86b1bf5b2a78851b5986b8
David Whittingham <DavidWhittingham@users.noreply.github.com>
2016-06-20 17:23:12 +1000

    Added export tiles config to sddraft_cacheable

--- Source/arcpyext/publishing/_sddraft_cacheable.py
+++ Source/arcpyext/publishing/_sddraft_cacheable.py
@@ -13,3 +13,38 @@ class SDDraftCacheable(SDDraftBase):
         value = self._editor.value_to_boolean(value)
 
         self._editor.set_element_value(self._editor.get_first_element_by_tag("KeepExistingMapCache"), value)
+
+    @property
+    def export_tiles_allowed(self):
+        """Gets a boolean value that describes if the service should allow tiles to be exported."""
+        return self._editor.value_to_boolean(self._editor.get_element_value(self._export_tiles_allowed_element))
+
+    @export_tiles_allowed.setter
+    def export_tiles_allowed(self, value):
+        """Sets a boolean value that describes if the service should allow tiles to be exported."""
+        value = self._editor.value_to_boolean(value)
+
+        self._editor.set_element_value(self._export_tiles_allowed_element, value)
+
+    @property
+    def max_export_tiles_count(self):
+        """Gets an integer value that describes how many tiles can be expored."""
+        return int(self._editor.get_element_value(self._max_export_tiles_count_element))
+
+    @max_export_tiles_count.setter
+    def max_export_tiles_count(self, value):
+        """Sets an integer value that describes how many tiles can be expored."""
+        value = int(value)
+        
+        if value < 0:
+            raise ValueError("Max Export Tiles Count cannot be less than zero.")
+
+        self._editor.set_element_value(self._max_export_tiles_count_element, value)
+
+    @property
+    def _export_tiles_allowed_element(self):
+        return self._editor.get_value_element_by_key(self._config_props, "exportTilesAllowed")
+
+    @property
+    def _max_export_tiles_count_element(self):
+        return self._editor.get_value_element_by_key(self._config_props, "maxExportTilesCount")
--- Source/tests/publishing/sddraft_cacheable.py
+++ Source/tests/publishing/sddraft_cacheable.py
@@ -6,3 +6,22 @@ from .. helpers import *
 def test_keep_cache(sddraft, keep_cache, expected):
     sddraft.keep_cache = keep_cache
     assert sddraft.keep_cache == expected
+    
+@pytest.mark.parametrize(("export_tiles_allowed", "expected"), TRUEISH_TEST_PARAMS)
+def test_export_tiles_allowed(sddraft, export_tiles_allowed, expected):
+    sddraft.export_tiles_allowed = export_tiles_allowed
+    assert sddraft.export_tiles_allowed == expected
+
+@pytest.mark.parametrize(("max_export_tiles_count", "ex"), [
+    (0, None),
+    (100, None),
+    (99999, None),
+    (-10, ValueError)
+])
+def test_max_export_tiles_count(sddraft, max_export_tiles_count, ex):
+    if ex != None:
+        with pytest.raises(ex):
+            sddraft.max_export_tiles_count = max_export_tiles_count
+    else:
+        sddraft.max_export_tiles_count = max_export_tiles_count
+        assert sddraft.max_export_tiles_count == max_export_tiles_count
