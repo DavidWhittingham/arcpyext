e90de6893437cdcf5c8583df395b0ec095b12895
Adam Kerz <adam.kerz@dnrm.qld.gov.au>
2017-04-06 15:01:17 +1000

    Calculating the path to the sample toolbox better

--- Source/tests/toolbox/test_python_toolbox.py
+++ Source/tests/toolbox/test_python_toolbox.py
@@ -1,11 +1,14 @@
+import os
 import arcpyext
 import pytest
 
 
 
+TOOLBOX_PATH=os.path.abspath("{0}/../samples/test_python_toolbox.pyt".format(os.path.dirname(__file__)))
+
+
 def test_update_toolbox_descriptions():
-    toolboxPath='samples/test_python_toolbox.pyt'
-    pt=arcpyext.toolbox.PythonToolbox(toolboxPath)
+    pt=arcpyext.toolbox.PythonToolbox(TOOLBOX_PATH)
     pt.load()
     pt.load_xml()
     pt.apply_toolbox_descriptions()
