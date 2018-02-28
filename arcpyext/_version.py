8e2f89769d0df256c2595c99e5dd07b71b87399f
baternikd <daniel.baternik@dnrme.qld.gov.au>
2018-02-28 17:22:53 +1000

    Refactored to support aws and cd

--- arcpyext/_version.py
+++ arcpyext/_version.py
@@ -1,3 +1,3 @@
-__version__   = "0.5.21"
-__author__    = "David Whittingham; David Payne; Adam Kerz; Peter Reyne"
+__version__ = "0.6.0-alpha.1"
+__author__ = "David Whittingham; David Payne; Adam Kerz; Peter Reyne"
 __copyright__ = "Copyright (C) 2016 David Whittingham"
