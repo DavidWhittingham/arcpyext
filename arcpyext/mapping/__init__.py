49e3443bdb5922a7d92b8721961b8ed4c06a8805
BLANCHFIELD Chris <Chris.Blanchfield@daf.qld.gov.au>
2019-02-14 09:13:12 +1000

    Changes to test gdbs for quicker running

--- arcpyext/mapping/__init__.py
+++ arcpyext/mapping/__init__.py
@@ -1,7 +1,1 @@
-# explictly break if arcpy.mapping does not exist
-try:
-    import arcpy.mapping
-except (AttributeError, ImportError):
-    raise
-
 from ._mapping import change_data_sources, create_replacement_data_sources_list, list_document_data_sources, validate_map, compare
