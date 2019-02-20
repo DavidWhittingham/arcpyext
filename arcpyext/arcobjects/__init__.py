a2e94f9e7086693efa6122f53055aa72bffbd08f
Chris Blanchfield <48502250+christopherblanchfield@users.noreply.github.com>
2019-02-20 14:13:08 +1000

    Abandoning use of pro .net apis

--- arcpyext/arcobjects/__init__.py
+++ arcpyext/arcobjects/__init__.py
@@ -1,6 +1,2 @@
-# Determine the environment and get appropriate methods
-try:
-    import arcpy.mapping
-    from ._arcobjects import init_arcobjects_context, destroy_arcobjects_context, list_layers, mxd_exists, open_mxd
-except:
-    from ._proobjects import init_arcobjects_context, destroy_arcobjects_context, list_layers, project_exists, open_project, create_project
+from ._arcobjects import init_arcobjects_context, destroy_arcobjects_context, list_layers, mxd_exists, open_mxd
+
