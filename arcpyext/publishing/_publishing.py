0b935e6f7e70932b77096e3d48249677a6420cec
David Whittingham <DavidWhittingham@users.noreply.github.com>
2019-04-24 09:56:36 +1000

    Fixed Py2/3 compat imports, linted

--- arcpyext/publishing/_publishing.py
+++ arcpyext/publishing/_publishing.py
@@ -4,8 +4,8 @@
 # Python 2/3 compatibility
 # pylint: disable=wildcard-import,unused-wildcard-import,wrong-import-order,wrong-import-position
 from __future__ import (absolute_import, division, print_function, unicode_literals)
-from future.builtins import *
 from future.builtins.disabled import *
+from future.builtins import *
 from future.standard_library import install_aliases
 install_aliases()
 # pylint: enable=wildcard-import,unused-wildcard-import,wrong-import-order,wrong-import-position
