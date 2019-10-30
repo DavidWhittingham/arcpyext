ba3f8fbe22cc835df14d6b6c949e733e3448f1b9
David Whittingham <DavidWhittingham@users.noreply.github.com>
2019-10-31 07:21:04 +1000

    Removed import that no longer exists

--- arcpyext/publishing/__init__.py
+++ arcpyext/publishing/__init__.py
@@ -4,5 +4,4 @@ try:
                               convert_toolbox_to_service_draft)
 except:
     from ._publishing import (convert_pro_map_to_service_draft as convert_map_to_service_draft,
-                              convert_service_draft_to_staged_service,
                               convert_toolbox_to_service_draft)
