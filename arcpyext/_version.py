7001ab47295b82b8995487603fdb7d2a64713199
David Whittingham <DavidWhittingham@users.noreply.github.com>
2019-11-06 10:28:36 +1000

    Bumped version number for release

--- arcpyext/_version.py
+++ arcpyext/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.6.4"
+__version__ = "0.6.5"
 __author__ = "David Whittingham; David Payne; Adam Kerz; Peter Reyne; Daniel Baternik; Chris Blanchfield"
 __copyright__ = "Copyright (C) 2019 David Whittingham"
