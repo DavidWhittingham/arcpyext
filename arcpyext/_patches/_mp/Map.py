c0687371721f7cf6a93a615be97770af4e2dd383
Gary Bagnall <gary.bagnall@resources.qld.gov.au>
2022-05-26 15:39:07 +1000

    Added py2 version of hiding files

--- arcpyext/_patches/_mp/Map.py
+++ arcpyext/_patches/_mp/Map.py
@@ -4,6 +4,7 @@ This module contains patches to Esri's *arcpy._mp.Map* class.  These patches may
 directly in the *arcpy._mp.Map* class.
 """
 
+import sys
 import arcpy
 from arcpy._mp import Layer, Map
 from arcpyext._patches.patches import get_cim_version
@@ -21,35 +22,77 @@ def add_set_layer_visibility():
             layers_list = [id.strip() for id in layers_list.split(",")]
         except:
             raise Exception("Could not parse layers list to determine layer visiblity: %s", layer_visibility)
+        
+        
+        def set_layer_visibility_2():
+            from arcpyext.mapping import _mapping2 as _mh
+            ao_map_document = self
+            for map_frame in _mh._native_list_maps(ao_map_document):
+                layer_list = _mh._native_list_layers(ao_map_document, map_frame)
+                
+                for index, layer in enumerate(layer_list):
+                    layer_desc = _mh._native_describe_layer(layer)
+                    layer_id = str(layer_desc["serviceId"])
 
-        for layer in self.listLayers():
-            layer = layer  # type: Layer
-            cim_version = get_cim_version()
-            layer_id = str(layer.getDefinition(cim_version).serviceLayerID)
+                    if caseless_equal(layers_op, "show"):
+                        # make only the specified layers visible
+                        if layer_id in layers_list:
+                            layer_list[index]["layer"].Visible = True
+                        else:
+                            layer_list[index]["layer"].Visible = False
+                    elif caseless_equal(layers_op, "hide"):
+                        # make all layers visible except those specified
+                        if layer_id in layers_list:
+                            layer_list[index]["layer"].Visible = False
+                        else:
+                            layer_list[index]["layer"].Visible = True
+                    elif caseless_equal(layers_op, "include"):
+                        # make the specified layers visible, along with the defaults
+                        if layer_id in layers_list:
+                            layer_list[index]["layer"].Visible = True
+                    elif caseless_equal(layers_op, "exclude"):
+                        # make the specified layers invisible, along with the defaults
+                        if layer_id in layers_list:
+                            layer_list[index]["layer"].Visible = False
+                    else:
+                        raise ValueError("Layer visibility operation not recognized.")
 
-            if caseless_equal(layers_op, "show"):
-                # make only the specified layers visible
-                if layer_id in layers_list:
-                    layer.visible = True
+        
+        def set_layer_visibility_3():
+            for layer in self.listLayers():
+                layer = layer  # type: Layer
+                cim_version = get_cim_version()
+                layer_id = str(layer.getDefinition(cim_version).serviceLayerID)
+
+                if caseless_equal(layers_op, "show"):
+                    # make only the specified layers visible
+                    if layer_id in layers_list:
+                        layer.visible = True
+                    else:
+                        layer.visible = False
+                elif caseless_equal(layers_op, "hide"):
+                    # make all layers visible except those specified
+                    if layer_id in layers_list:
+                        layer.visible = False
+                    else:
+                        layer.visible = True
+                elif caseless_equal(layers_op, "include"):
+                    # make the specified layers visible, along with the defaults
+                    if layer_id in layers_list:
+                        layer.visible = True
+                elif caseless_equal(layers_op, "exclude"):
+                    # make the specified layers invisible, along with the defaults
+                    if layer_id in layers_list:
+                        layer.visible = False
                 else:
-                    layer.visible = False
-            # raise NotImplementedError()
-            elif caseless_equal(layers_op, "hide"):
-                # make all layers visible except those specified
-                if layer_id in layers_list:
-                    layer.visible = False
-                else:
-                    layer.visible = True
-            elif caseless_equal(layers_op, "include"):
-                # make the specified layers visible, along with the defaults
-                if layer_id in layers_list:
-                    layer.visible = True
-            elif caseless_equal(layers_op, "exclude"):
-                # make the specified layers invisible, along with the defaults
-                if layer_id in layers_list:
-                    layer.visible = False
-            else:
-                raise ValueError("Layer visibility operation not recognized.")
+                    raise ValueError("Layer visibility operation not recognized.")
+        
+        
+        ARCPY_2 = sys.version_info[0] < 3
+        if ARCPY_2:
+            set_layer_visibility_2()
+        else:
+            set_layer_visibility_3()
 
         return self
 
